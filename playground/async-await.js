452a1c1d839475452cadc403e34f05f11b66bacc
jgabrielgv <jgmezvargas@gmail.com>
2018-05-13 16:15:44 -0600

    First commit

---
+++ playground/async-await.js
@@ -0,0 +1,28 @@
+const getItems = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['Mobile', 'Mac', 'Pen', 'Book']);
+    }, 1000);
+  });
+};
+
+const processRequest = async () => {
+  const items1 = await getItems();
+  const items2 = await getItems();
+  const items3 = await getItems();
+  const items4 = await getItems();
+  const items5 = await getItems();
+  const items6 = await getItems();
+  const items7 = await getItems();
+  return [items1, items2, items3, items4, items5, items6, items7];
+};
+
+processRequest()
+.then(results => console.log(results))
+.catch(err => console.error(err));
+
+// getItems().then(results => {
+//   console.log(results);
+// }).catch(err => {
+//   console.err(err);
+// });
---
+++ server.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const app = new Koa();
+const PORT = 4000;
+
+app.use(async (ctx, next) => {
+  console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  console.log(`2nd middleware`);
+  return await next();
+});
+
+app.use(async ctx => {
+  ctx.body = 'Hello world!';
+});
+
+app.listen(PORT);
+console.log(`Server is listening on PORT ${PORT}`);
