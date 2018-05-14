c7354d47e9e3ad61561ff1f5da558a0f84ad58c8
= <jgmezvargas@gmail.com>
2018-05-14 15:55:51 -0600

    Adding router

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 const PORT = 4000;
 
-app.use(async (ctx, next) => {
-  console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
-  await next();
+// main entry
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Welcome to koa Application';
 });
 
-app.use(async (ctx, next) => {
-  console.log(`2nd middleware`);
-  return await next();
+// posts
+
+const posts = [
+  {
+    "id": '1',
+    "name": "Nodejs Developer",
+    "content": "test"
+  },
+  {
+    "id": '2',
+    "name": "C# Developer",
+    "content": "test"
+  },
+  {
+    "id": '3',
+    "name": "Java Developer",
+    "content": "test"
+  },
+];
+
+router.get('/posts', (ctx, next) =>{
+  ctx.body = posts;
 });
 
-app.use(async ctx => {
-  ctx.body = 'Hello world!';
-});
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+// app.use(async (ctx, next) => {
+//   console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
+//   await next();
+// });
+
+// app.use(async (ctx, next) => {
+//   console.log(`2nd middleware`);
+//   return await next();
+// });
+
+// app.use(async ctx => {
+//   ctx.body = 'Hello world!';
+// });
 
 app.listen(PORT);
 console.log(`Server is listening on PORT ${PORT}`);
